c35c880a64692e191036760b4f17334abc777f10
NawresB~ <nawresbenali33@gmail.com>
2023-05-09 11:31:38 +0200

    done with my task

--- CraftSeeker/Navigation.js
+++ CraftSeeker/Navigation.js
@@ -4,6 +4,8 @@ import {NavigationContainer} from '@react-navigation/native';
 import Edit from './components/screens/WorkerProfil/Edit';
 import WorkerProfil from './components/screens/WorkerProfil/WorkerProfil';
 import Dashboard from './components/screens/worker/WorkersDashBoard/DashBoard'
+import EditProfil from './components/screens/client/Profil/EditProfil';
+import Profil from './components/screens/client/Profil/Profil';
 const Stack = createNativeStackNavigator();
 
 
@@ -16,11 +18,28 @@ export default function Navigation() {
   return (
     <NavigationContainer>
     <Stack.Navigator>
+
+    <Stack.Screen name = "Profil" component={Profil}/>
+      <Stack.Screen name="EditProfil" component={EditProfil} />
       {/* <Stack.Screen name="Dashboard" component={Dashboard} /> */}
       <Stack.Screen name = "WorkerProfil" component={WorkerProfil}/>
       <Stack.Screen name="Edit" component={Edit} />
        
+  
+
+  
+     
+      
+
     </Stack.Navigator>
+
+
+
   </NavigationContainer>
+
+
+
+
+
   )
 }
